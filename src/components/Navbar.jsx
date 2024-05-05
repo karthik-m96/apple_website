92ec02b156679315f81bd23c151adfe395003e9d
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2024-05-05 20:51:43 +0530

    comit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {navLists} from "../constants"
 
 const Navbar = () => {
     return (
-        <header className="w-full py-10 sm:px-10 px-5 flex justify-between items-center">
+        <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
             <nav className="flex w-full screen-max-width">
                 <img src={appleImg} alt="apple" width={12} height={18} />
                 <div className="flex flex-1 justify-center max-sm:hidden">
@@ -13,7 +13,7 @@ const Navbar = () => {
                         </div>
                     ))}
                 </div>
-                <div className="">
+                <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 cursor-pointer">
                     <img src={searchImg} alt="search" width={18} height={18} />
                     <img src={bagImg} alt="bag" width={18} height={18} />
                 </div>
